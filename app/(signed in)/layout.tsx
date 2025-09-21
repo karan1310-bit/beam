d390a6a7e788079def65a4e861acdee92dc7bd8c
karan <karanbhati1310@gmail.com>
2025-09-21 09:57:24 +0530

    sidebar and chat 1/2

--- app/(signed in)/layout.tsx
+++ app/(signed in)/layout.tsx
@@ -4,12 +4,45 @@ import UserSyncWrapper from '@/components/UserSyncWrapper'
 import streamClient from '@/lib/stream'
 import React from 'react'
 import { Chat } from 'stream-chat-react'
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import Link from 'next/link'
 
 
 const Layout = ({ children } : { children: React.ReactNode }) => {
   return (
     <UserSyncWrapper>
-        <Chat client={streamClient}>{children}</Chat>
+        <Chat client={streamClient}>
+          <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "19rem",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4 font-DMregular">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <Link href="/dashboard" >
+          <h1 className="text-lg font-medium uppercase">Beamy</h1>
+          </Link>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+          </Chat>
     </UserSyncWrapper>
   )
 }
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,79 @@
+import { UserButton, useUser } from "@clerk/nextjs";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarGroup,
+} from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+import { ChannelList } from "stream-chat-react";
+import { ChannelFilters, ChannelOptions, ChannelSort } from "stream-chat";
+
+export function AppSidebar() {
+  const { user } = useUser();
+
+  const filters: ChannelFilters = {
+    members: { $in: [user?.id as string] },
+    type: { $in: ["messaging", "team"] }, // Show both 1-1 chats and group chats
+  };
+
+  const sort: ChannelSort = { last_message_at: -1 };
+
+  const options: ChannelOptions = { presence: true, state: true };
+
+  return (
+    <Sidebar variant="floating" {...(user ? {}: { isCollapsed: true })}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <div className="flex items-center justify-between w-full font-DMregular">
+                <div className="flex-col ">
+                  <span className="text-sm text-muted-foreground">Welcome back</span>
+                  <span className="text-md font-semibold ml-1">
+                    {user?.firstName} {user?.lastName}
+                  </span>
+                </div>
+                <UserButton signInUrl="/sign-in" />
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <Button className="w-full" variant="outline">
+                Start New Chat
+              </Button>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+
+        {/* Channels List */}
+        <SidebarMenu>
+          <ChannelList
+            filters={filters}
+            sort={sort}
+            options={options}
+            EmptyStateIndicator={() => (
+              <div className="flex flex-col items-center justify-center p-4">
+                <h2 className="text-xl opacity-20 text-text-muted-foreground mb-2">
+                  Ready to chat?
+                </h2>
+                <p className="text-sm text-muted-foreground leading-relaxed max-w-[200px] text-center">
+                  Your conversations will appear here once you start chatting with others.
+                </p>
+              </div>
+            )}
+          />
+        </SidebarMenu>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
